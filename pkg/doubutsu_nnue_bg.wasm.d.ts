d5ee8e47c68d8c8f0d27ced538394aa648ac338a
wasabi-eater <yusuke.akinari@gmail.com>
2026-07-02 08:24:11 +0900

    wasmをignoreから除外

---
+++ pkg/doubutsu_nnue_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_animalshogiwasm_free: (a: number, b: number) => void;
+export const animalshogiwasm_apply_human_drop: (a: number, b: number, c: number) => number;
+export const animalshogiwasm_apply_human_move: (a: number, b: number, c: number) => number;
+export const animalshogiwasm_get_board_string: (a: number) => [number, number];
+export const animalshogiwasm_get_turn: (a: number) => number;
+export const animalshogiwasm_get_winner: (a: number) => number;
+export const animalshogiwasm_new: () => number;
+export const animalshogiwasm_reset: (a: number) => void;
+export const animalshogiwasm_search_and_apply_move: (a: number, b: bigint) => [number, number];
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
